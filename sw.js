9e39235064dc516087d3976af4418442ec4b3860
olyashilina555 <o.olya.shilina@gmail.com>
2026-09-08 17:23:51 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,24 @@
-const CACHE = "feedboard-v2.0.0";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+const CACHE="feedboard-v3";
+const ASSETS=["./","./index.html","./manifest.webmanifest"];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
-self.addEventListener("activate", event => {
+self.addEventListener("activate",event=>{
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-    ))
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
   );
   self.clients.claim();
 });
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
   event.respondWith(
     fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      .then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
         return response;
       })
-      .catch(() => caches.match(event.request))
+      .catch(()=>caches.match(event.request))
   );
 });
