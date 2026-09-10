6120432658a4c18534da4394b0847850134aa734
olyashilina555 <o.olya.shilina@gmail.com>
2026-09-10 19:49:56 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
-const CACHE="feedboard-v3-3";
+const CACHE="feedboard-v3-5";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate",event=>{
   event.waitUntil(
-    caches.keys().then(keys=>Promise.all(
-      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
-    ))
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
   );
   self.clients.claim();
 });
-
 self.addEventListener("fetch",event=>{
   if(event.request.method!=="GET")return;
   event.respondWith(
